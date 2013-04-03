2de2d7cd920ab83480a0c33366732714bac0fc59
Nathan Totten <ntotten@microsoft.com>
2013-04-03 14:50:37 -0700

    Updated config

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ nconf.argv()
 var app = express();
 
 // all environments
-app.set('port', nconf.get("PORT") || 3000);
+app.set('port', nconf.get("PORT") || process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
